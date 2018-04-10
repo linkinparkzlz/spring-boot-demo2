b426503f88b95bf5ba1bbfa4de870842037a9f75
zoulvzhou <940933073@qq.com>
2018-04-10 10:11:48 +0800

    filter过滤器

---
+++ src/main/java/com/example/springbootdemo2/servlet/MyFilter.java
@@ -0,0 +1,78 @@
+package com.example.springbootdemo2.servlet;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(servletNames = "myServlet")
+public class MyFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+
+        ServletContext servletContext = httpServletRequest.getServletContext();
+
+        servletContext.log("/myServlet   was  filted");
+
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
